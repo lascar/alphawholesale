858c9e069302c504976811abb3b8a9a6a7a08570
pascal.carrie@gmail.com <pascal>
2018-11-08 10:01:21 +0100

    offers corrections

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -34,12 +34,12 @@ module OffersHelper
   def offer_form_hash(supplier_id, offer)
     if offer.id.nil?
       path = supplier_signed_in? ?
-       new_supplier_offer_path(offer.id, supplier_id: supplier_id) :
-       new_offer_path({id: offer.id, supplier_id: supplier_id})
+       supplier_offers_path(offer.id, supplier_id: supplier_id) :
+       offers_path({id: offer.id, supplier_id: supplier_id})
     else
       path = supplier_signed_in? ?
-       supplier_offer_path(offer.id, supplier_id: supplier_id) :
-       offer_path({id: offer.id, supplier_id: supplier_id})
+        "/suppliers/" + supplier_id.to_s + "/offers/" + offer.id.to_s :
+        "/offers/" + offer.id.to_s + "?supplier_id=" + supplier_id.to_s
     end
     method = offer.persisted? ? "patch" : "post"
     {url: path, model: offer, local: true, method: method}
