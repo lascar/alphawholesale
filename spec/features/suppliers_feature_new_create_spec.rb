f24aaace95e6886cf7ca783d9e03bd998469be58
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2019-01-29 18:40:52 +0100

    use scenario in features

--- spec/features/suppliers_feature_new_create_spec.rb
+++ spec/features/suppliers_feature_new_create_spec.rb
@@ -19,40 +19,35 @@ RSpec.describe 'Suppliers Feature new', type: :feature do
     # TEST as a broker
     # TEST when a new supplier is submited
     # TEST then a message of success is sent
-    describe 'as a broker' do
-      before :each do
-        sign_in(broker1)
-      end
-
-      it 'assigns a new supplier' do
-        visit new_supplier_path
-        expect(page).to have_xpath("//form[@action='/suppliers'
-                                   and @method='post']")
-        expect(page).to have_field({name: 'supplier[approved]'})
-        fill_in('supplier[identifier]', :with => supplier_hash[:identifier])
-        fill_in('supplier[email]', :with => supplier_hash[:email])
-        fill_in('supplier[entreprise_name]',
-                :with => supplier_hash[:entreprise_name])
-        fill_in('supplier[tin]', :with => supplier_hash[:tin])
-        fill_in('supplier[street_and_number]',
-                :with => supplier_hash[:street_and_number])
-        fill_in('supplier[city]', :with => supplier_hash[:city])
-        fill_in('supplier[postal_code]', :with => supplier_hash[:postal_code])
-        fill_in('supplier[state]', :with => supplier_hash[:state])
-        fill_in('supplier[country]', :with => supplier_hash[:country])
-        fill_in('supplier[telephone_number1]',
-                :with => supplier_hash[:telephone_number1])
-        fill_in('supplier[telephone_number2]',
-                :with => supplier_hash[:telephone_number2])
-        fill_in('supplier[password]', :with => supplier_hash[:password])
-        fill_in('supplier[password_confirmation]',
-                :with => supplier_hash[:password])
-        find('[name=commit]').click
-        expect(page).to have_content(
-          I18n.t('controllers.suppliers.successfully_created'))
-        expect(page.current_url).to eq(
-          'http://www.example.com/suppliers/' + Supplier.last.id.to_s)
-      end
+    scenario "broker creates a new supplier" do
+      sign_in(broker1)
+      visit new_supplier_path
+      expect(page).to have_xpath("//form[@action='/suppliers'
+                                 and @method='post']")
+      expect(page).to have_field({name: 'supplier[approved]'})
+      fill_in('supplier[identifier]', :with => supplier_hash[:identifier])
+      fill_in('supplier[email]', :with => supplier_hash[:email])
+      fill_in('supplier[entreprise_name]',
+              :with => supplier_hash[:entreprise_name])
+      fill_in('supplier[tin]', :with => supplier_hash[:tin])
+      fill_in('supplier[street_and_number]',
+              :with => supplier_hash[:street_and_number])
+      fill_in('supplier[city]', :with => supplier_hash[:city])
+      fill_in('supplier[postal_code]', :with => supplier_hash[:postal_code])
+      fill_in('supplier[state]', :with => supplier_hash[:state])
+      fill_in('supplier[country]', :with => supplier_hash[:country])
+      fill_in('supplier[telephone_number1]',
+              :with => supplier_hash[:telephone_number1])
+      fill_in('supplier[telephone_number2]',
+              :with => supplier_hash[:telephone_number2])
+      fill_in('supplier[password]', :with => supplier_hash[:password])
+      fill_in('supplier[password_confirmation]',
+              :with => supplier_hash[:password])
+      find('[name=commit]').click
+      expect(page).to have_content(
+        I18n.t('controllers.suppliers.successfully_created'))
+      expect(page.current_url).to eq(
+        'http://www.example.com/suppliers/' + Supplier.last.id.to_s)
     end
 
   end
