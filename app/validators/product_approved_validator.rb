f59737295b2fe7ec252604f69975c41d81c13d4a
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2019-10-28 08:59:11 +0100

    validate in model for attached product
    attached product fix some in show customer and supplier

--- app/validators/product_approved_validator.rb
+++
@@ -1,7 +0,0 @@
-class ProductApprovedValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.product.approved
-      record.errors[:base] << I18n.t('activerecord.errors.models.attached_product.product.not_approved', product: record.product.name)
-    end
-  end
-end
