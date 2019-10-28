dc54ff8b29daacf992b3eb0e01817fb5dfdd713b
pascal.carrie@gmail.com <pascal.carrie@gmail.com>
2019-10-28 08:59:11 +0100

    validate in model for attached product

--- app/models/attached_product.rb
+++ app/models/attached_product.rb
@@ -4,7 +4,30 @@ class AttachedProduct < ApplicationRecord
   belongs_to :aspect, optional: true
   belongs_to :packaging, optional: true
   belongs_to :attachable, polymorphic: true
-  validates_with BrokerAttachedProductValidator
-  validates_with ProductApprovedValidator
-  validates_with AlreadyAttachedValidator
+  validate :broker_attached
+  validate :product_approved
+  validate :already_attached
+
+  private
+  def already_attached
+    unless AttachedProduct.unscoped.find_by(product_id: product_id,
+        variety_id: variety&.id, aspect_id: aspect&.id, packaging_id: packaging&.id,
+        attachable_type: attachable_type, attachable_id: attachable_id).nil?
+      errors[:base] << I18n.t('activerecord.errors.models.attached_product.uniqueness')
+    end
+  end
+
+  def product_approved
+    unless product.approved
+      errors[:base] << I18n.t('activerecord.errors.models.attached_product.product.not_approved', product: product.name)
+    end
+  end
+
+  def broker_attached
+    unless attachable_type == 'Broker' || AttachedProduct.find_by(product_id: product_id,
+        variety_id: variety_id, aspect_id: aspect_id, packaging_id: packaging_id,
+        attachable_type: 'Broker')
+      errors[:base] << I18n.t('activerecord.errors.models.attached_product.not_attachable')
+    end
+  end
 end
